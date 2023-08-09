e67e574193a8f5649519bb2ca0863a57da48772e
vaberry <vincent.berry11@gmail.com>
2023-08-08 20:16:37 -0400

    e

--- funcs.py
+++ funcs.py
@@ -1,8 +1,11 @@
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+from selenium import webdriver
 from bs4 import BeautifulSoup
-import requests
+import pinecone
 import re
 import os
-import pinecone
 
 city_dict = {
     'chicago': 'il--chicago',
@@ -12,13 +15,6 @@ city_dict = {
 def clean_string(input_string):
     cleaned_string = re.sub(r'[ /]', '_', input_string).lower()
     return cleaned_string
-
-def make_api_call(event_id):
-    base_url = f"https://www.eventbriteapi.com/v3/events/{event_id}"
-    headers = {"Authorization": f"Bearer {os.getenv('EVENTBRITE_API_KEY')}"}
-    response = requests.get(base_url, headers=headers)
-    data = response.json()
-    return data
 
 def make_event_txt(driver, event):
     event_name = event.find('h2', class_='Typography_root__4bejd #3a3247 Typography_body-lg__4bejd event-card__clamp-line--two Typography_align-match-parent__4bejd').text
@@ -35,7 +31,23 @@ def make_event_txt(driver, event):
         f.write(f'Event ID: {event_id}\n')
         f.write(f'Event Description: {event_description}')
 
-def start_pinecone():
+def scrape_events():
+    driver = webdriver.Chrome()
+    for _, city_code in city_dict.items():
+        for i in range(1, 2):
+            try:
+                driver.get(f"https://www.eventbrite.com/d/{city_code}/all-events/?page={i}")
+                driver.implicitly_wait(10)
+                soup = BeautifulSoup(driver.page_source, 'html.parser')
+                events_list = soup.find_all('li', class_='search-main-content__events-list-item')
+                for event in events_list:
+                    make_event_txt(driver, event)
+            except Exception as e:
+                print(e)
+                break
+    driver.quit()
+
+def load_vectors():
     OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
     PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
     PINECONE_ENV = os.getenv("PINECONE_ENV")
@@ -43,3 +55,19 @@ def start_pinecone():
         api_key=PINECONE_API_KEY,
         environment=PINECONE_ENV
     )
+    for file in os.listdir('events')[0:11]:
+        with open(f'events/{file}') as f:
+            try:
+                event = f.read()
+                text_splitter = CharacterTextSplitter(   
+                    separator = "\n\n",
+                    length_function = len,
+                )
+                texts = text_splitter.create_documents([event])
+                index = pinecone.Index("events-index")
+                embeddings = OpenAIEmbeddings()
+                vectorstore = Pinecone(index, embeddings.embed_query, "text")
+                vectorstore.add_documents(texts)
+                print('vectored')
+            except Exception as e:
+                print(e)
--- scrape.py
+++ scrape.py
@@ -1,20 +1,4 @@
-from dotenv import load_dotenv
-from selenium import webdriver
-from bs4 import BeautifulSoup
 import funcs as funcs
-load_dotenv()
 
-driver = webdriver.Chrome()
-for city, city_code in funcs.city_dict.items():
-    for i in range(1, 2):
-        try:
-            driver.get(f"https://www.eventbrite.com/d/{city_code}/all-events/?page={i}")
-            driver.implicitly_wait(10)
-            soup = BeautifulSoup(driver.page_source, 'html.parser')
-            events_list = soup.find_all('li', class_='search-main-content__events-list-item')
-            for event in events_list:
-                funcs.make_event_txt(driver, event)
-        except Exception as e:
-            print(e)
-            break
-driver.quit()
+funcs.scrape_events()
+funcs.load_vectors()
